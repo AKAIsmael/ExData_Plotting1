1d9a36c5b5e1c37d72fa93180d6545b96242798b
AKAIsmael <abdelrahmankamal92@gmail.com>
2020-07-07 08:40:41 -0400

    Updated

--- Plot2.R
+++ Plot2.R
@@ -6,7 +6,7 @@ unlink(temp)
 hhpc <-  hhpc[(hhpc$Date=="1/2/2007") | (hhpc$Date =="2/2/2007" ),]
 hhpc$Global_active_power<- as.numeric(hhpc$Global_active_power)
 hhpc$tme <- dmy_hms(paste(hhpc$Date, " ", hhpc$Time))
+png("Plot2.png")
 plot(x=hhpc$tme ,y=hhpc$Global_active_power,type ="l", ylab="Global Active Power (Kilowatts)", xlab = "")
-dev.copy(png,"Plot2.png")
 dev.off()
 
