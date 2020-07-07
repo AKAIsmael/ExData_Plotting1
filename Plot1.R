029ee05713047c834e5f0f8c26a527083a0dac45
AKAIsmael <abdelrahmankamal92@gmail.com>
2020-07-07 08:41:54 -0400

    Updated

--- Plot1.R
+++ Plot1.R
@@ -6,6 +6,6 @@ unlink(temp)
 hhpc <-  hhpc[(hhpc$Date=="1/2/2007") | (hhpc$Date =="2/2/2007" ),]
 hhpc$Global_active_power<- as.numeric(hhpc$Global_active_power)
 hhpc$Date <- dmy(hhpc$Date)
+png("Plot1.png")
 hist(hhpc$Global_active_power, col = "red",ylim=c(0,1200),main="Global Active Power", ylab="Frequency", xlab="Global Active Power (Kilowatts)")
-dev.copy(png,"Plot1.png")
 dev.off()
